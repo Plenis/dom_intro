7caf3847018b32afd7587bc87f2a0436e1b246fc
Plenis <plenissino@gmail.com>
2019-05-29 11:46:18 +0200

    text bill complete

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,12 +1,45 @@
 // get a reference to the textbox where the bill type is to be entered
-
+var billTypeText = document.querySelector(".billTypeText");
+var textTotalAddBtn = document.querySelector(".addToBillBtn")
 //get a reference to the add button
 
 //create a variable that will keep track of the total bill
+var callsTotalElemOne = document.querySelector(".callTotalOne");
+var smsTotalElemOne = document.querySelector(".smsTotalOne");
+var totalCostElemOne = document.querySelector(".totalOne");
 
-//add an event listener for when the add button is pressed
+// these variables are global and defined outside of the Add button event listener.
+var callsTotal = 0;
+var smsTotal = 0;
+var totalCost = 0;
 
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+
+function textBillTotal(){
+    // get the value entered in the billType textfield
+    var billTypeEntered = billTypeText.value.trim();
+    // update the correct total
+    if (billTypeEntered === "call"){
+        callsTotal += 2.75 
+        
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    
+    //update the totals that is displayed on the screen.
+    callsTotalElemOne.innerHTML = callsTotal.toFixed(2);
+    smsTotalElemOne.innerHTML = smsTotal.toFixed(2);
+     totalCost = callsTotal + smsTotal;
+    totalCostElemOne.innerHTML = totalCost.toFixed(2);
+
+
+    if (totalCost >= 50){
+        // adding the danger class will make the text red
+        totalCostElemOne.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalCostElemOne.classList.add("warning");
+    }
+}
+
+textTotalAddBtn.addEventListener('click', textBillTotal);
